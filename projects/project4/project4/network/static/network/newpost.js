5790144ef3f8366b8368717c65116bd3f4670acc
Kevin DeAngeles <kevindeangelesjr@gmail.com>
2020-10-10 11:01:10 -0400

    10-10-2020

--- projects/project4/project4/network/static/network/newpost.js
+++ projects/project4/project4/network/static/network/newpost.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Use buttons to toggle between views
     document.querySelector('#new-post-btn').addEventListener('click', new_post);
+    document.querySelector('#new-post-cancel').addEventListener('click', cancel_post);
   });
 
 function new_post(){
 
     document.querySelector('#new-post').style.display = 'block';
+}
+
+function cancel_post(){
+
+  document.querySelector('#new-post').style.display = 'none';
 }
